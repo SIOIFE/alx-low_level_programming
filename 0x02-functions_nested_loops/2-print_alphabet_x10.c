75867b425a616503d7e55844bb19344bae37511e
SIOIFE <samuelifeoluwa20@yahoo.com>
2023-03-21 13:18:32 -0700

    3-islower

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Return: alway 0 (success)
+ */
+void print_alphabet_x10(void)
+{
+	int n;
+	int m;
+
+	for (m = 1; <= 10; n++)
+	{
+		for (m = 97; m <= 122; m++)
+		{
+			_putchar(m);
+		}
+		_putchar('\n');
+		return (0);
+	}
+}

783e83c048525bd983d02801579115e15227f316
SIOIFE <samuelifeoluwa20@yahoo.com>
2023-03-27 16:26:52 -0700

    4-print_rev

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,3 +1,24 @@
 #include "main.h"
 /**
- *
+ * print_rev - prints a string, in reverse, followed by a new line
+ * @s: string
+ * return: 0
+ */
+void print_rev(char *s)
+{
+	int longi = 0;
+	int m;
+
+	while (*s != '\n')
+	{
+		longi++;
+		s--;
+	}
+	s--;
+	for (m = longi; m > 0; m--)
+	{
+		_putchar(*s);
+		s--;
+	}
+	_putchar('\n');
+}

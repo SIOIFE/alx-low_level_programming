1e53be782dd6a99580ba47773651db554cd898c0
SIOIFE <samuelifeoluwa20@yahoo.com>
2023-03-29 15:25:30 -0700

    0-strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,12 +1,13 @@
 #include "main.h"
 
+
 /**
  * _strcat - concatenates two strings
  * @dest: copy to
  * @src: copy from
  * Return: pointer to dest
  */
-char *_strcat(char *dest, char *src);
+char *_strcat(char *dest, char *src)
 {
 	int i;
 	int j;

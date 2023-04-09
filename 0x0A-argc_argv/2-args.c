ebe3556768ba168b6f4fec44be9ad1459113f055
SIOIFE <samuelifeoluwa20@yahoo.com>
2023-04-09 05:20:47 -0700

    args

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include "main.h"
+
+/**
+ * main - prints all arguments it receives
+ * @argv: Arguments vecto in an array
+ * @argc: Counts of arguments
+ *
+ * Return: On success 0
+ */
+int main(int argc, char *argv[])
+{
+	int a;
+
+	for (a = 0; a < argc; a++)
+	{
+		printf("%s\n", argv[a]);
+	}
+	return (0);
+}

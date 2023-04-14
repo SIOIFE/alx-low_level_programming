ecb10b30cbe2133fb20a0b1ad88e691bf79c5733
SIOIFE <samuelifeoluwa20@yahoo.com>
2023-04-13 21:16:15 -0700

    1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -11,7 +11,7 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *s;
-	unsigned int i = 0, j = 0, len1 = 0, len2 = 0;
+	unsigned int a = 0, b = 0, len1 = 0, len2 = 0;
 
 	while (s1 && s1[len1])
 		len1++;
@@ -23,15 +23,14 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		s = malloc(sizeof(char) * (len1 + len2 + 1));
 	if (!s)
 		return (NULL);
-	while (i < len 1)
-	{
-		s[i] = s1[i];
-		i++;
-	}
+	while (a < len 1)
+		s[a] = s1[a];
+	a++;
 	while (n < len2 && i < (len1 + n))
-		s[i++] = s2[j++];
+		s[a++] = s2[b++];
 	while (n >= len2 && i < (len1 + len2))
-		s[i++] = s2[j++];
-	s[i] = '\0';
+		s[a++] = s2[b++];
+	s[a] = '\0';
+
 	return (s);
 }

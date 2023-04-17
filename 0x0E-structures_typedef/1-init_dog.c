474011a4dcfc369e81128001816baf4726e064c1
SIOIFE <samuelifeoluwa20@yahoo.com>
2023-04-17 09:46:30 -0700

    print dog

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -8,11 +8,11 @@
  */
 void print_dog(struct dog *d)
 {
-	if (d == 0)
+	if (d == NULL)
 		return;
-	if (d->name == 0)
+	if (d->name == NULL)
 		d->name = "(nil)";
-	if (d->owner == 0)
+	if (d->owner == NULL)
 		d->owner = "(nil)";
 	printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
 }

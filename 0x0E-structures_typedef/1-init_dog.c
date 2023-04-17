e95aa105357db01ac1b8997b3b930687dc872e67
SIOIFE <samuelifeoluwa20@yahoo.com>
2023-04-17 09:27:16 -0700

    c-structures

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+/**
+ * print_dog - prints a struct dog
+ * @d: struct dog to print
+ */
+void print_dog(struct dog *d)
+{
+	if (d == 0)
+		return;
+	if (d->name == 0)
+		d->name = "(nil)";
+	if (d->owner == 0)
+		d->owner = "(nil)";
+	printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
+}
